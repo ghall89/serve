23c67c955764e988d28c7ea70f8a2011c7f6c4ad
Graham Hall <graham@ghall.dev>
2025-11-03 10:23:14 -0500

    prevent browser cacheing

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
 )
 
 func main() {
-	path := flag.String("path", ".", "Path of directory to serve")
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	path := flag.String("path", cwd, "Path of directory to serve")
 	port := flag.Int("port", 8080, "Port to serve on")
 
-	err := serveDir(path, port)
-	if err != nil {
+	fmt.Printf("Serving %s\n", *path)
+
+	if err := serveDir(path, port); err != nil {
 		log.Fatal(err)
 	}
 }
--- cmd/server.go
+++ cmd/server.go
@@ -6,10 +6,9 @@ import (
 )
 
 func serveDir(d *string, p *int) error {
-	var err error
-
 	fs := http.FileServer(http.Dir(*d))
-	http.Handle("/", fs)
+
+	http.Handle("/", noCache(fs))
 
 	port, err := getPort(p)
 	if err != nil {
@@ -17,10 +16,18 @@ func serveDir(d *string, p *int) error {
 	}
 
 	fmt.Printf("Listening on http://localhost%s", port)
-	err = http.ListenAndServe(port, nil)
-	if err != nil {
+	if err := http.ListenAndServe(port, nil); err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func noCache(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+		w.Header().Set("Pragma", "no-cache")
+		w.Header().Set("Expires", "0")
+		next.ServeHTTP(w, r)
+	})
 }
